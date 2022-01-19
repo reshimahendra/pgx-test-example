053db1f8c4742a284c576d5f169be903afab5f7f
Reshi E520 <reshimahendra@gmail.com>
2022-01-19 23:37:45 +0800

    make account accessible on local server

--- main.go
+++ main.go
@@ -1,10 +1,57 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"pgxtest/account"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    // gin.SetMode(gin.S)
-    r := gin.Default()
+    // separate the code from the 'main' function so we can test it.
+    // all code that available in main function were not testable
+    Run()
+}
+
+func Run() {
+    // prepare gin
+    // uncomment below mode if want to get back to default debug mode
+    gin.SetMode(gin.ReleaseMode)
+
+    // gin with default setup
+    r := gin.New()
+    // r.Use(gin.Logger())
+    r.Use(gin.Recovery())
+
+    // prepare database, remember to create "golangtest" database first
+    dbPool, _, err := account.NewDBPool(account.DatabaseConfig{
+        Username : "golang",
+        Password : "golang",
+        Hostname : "localhost",
+        Port : "5432",
+        DBName : "golangtest",
+    })
+    
+    defer dbPool.Close()
+
+    // log for error if error occur while connecting to the database
+    if err != nil {
+        log.Fatalf("unexpected error while tried to connect to database: %v\n", err)
+    }
+
+    // datastore := account.NewDatastore(dbPool)
+    accDB := account.NewDatabase(dbPool)
+    accService := account.NewAccountService(accDB)
+    accAPI := account.NewAccountHandler(accService)
+
+    // prepare router
+    // main group api endpoint url : http://domain.com/v1
+    v1 := r.Group("/v1")
+
+    // account app group api endpoint : http://domainname.com/v1/account
+    accRouter := v1.Group("/account")
+    accRouter.POST("/", accAPI.UserCreateHandler)
+    accRouter.GET("/:id", accAPI.UserGetHandler)
 
     r.Run(":8000")
 }
